0f76c0fe162c6a521df8c5c6e691ced2acc21b51
Nismit <nismit.dev@gmail.com>
2022-05-01 18:01:11 -0700

    First commit

--- src/hooks/useTimeline.ts
+++ src/hooks/useTimeline.ts
@@ -1,4 +1,8 @@
 import { useState, useRef, useEffect, useCallback } from "preact/hooks";
+<<<<<<< HEAD
+=======
+import { RECORDING_STATUS, RECORDING } from "./const";
+>>>>>>> ac3eb05 (First commit)
 
 const TOTAL_FRAMES = 600;
 
@@ -6,9 +10,18 @@ const TOTAL_FRAMES = 600;
 // when drag range input, the frame update should be stopped
 export const useTimeline = () => {
   const [time, setTime] = useState(0);
+<<<<<<< HEAD
   const [recording, setRecording] = useState(false);
   const [isRunning, setIsRunning] = useState(false);
   const [totalFrames, setTotalFrames] = useState(TOTAL_FRAMES);
+=======
+  const [recording, setRecording] = useState<RECORDING>(
+    RECORDING_STATUS.INITIAL
+  );
+  const [isRunning, setIsRunning] = useState(false);
+  const [totalFrames, setTotalFrames] = useState(TOTAL_FRAMES);
+  // const inputRef = useRef<HTMLInputElement>(null);
+>>>>>>> ac3eb05 (First commit)
   const rafRef = useRef<number>();
 
   const changeTime = useCallback(
@@ -39,6 +52,17 @@ export const useTimeline = () => {
     [setTotalFrames]
   );
 
+<<<<<<< HEAD
+=======
+  // useEffect(() => {
+  //   if (inputRef.current) {
+  //     // const canvas = inputRef.current;
+  //     // console.log("useEffect - canvas", canvas);
+  //     // console.log("useEffect - useTimeline", inputRef.current);
+  //   }
+  // }, [inputRef]);
+
+>>>>>>> ac3eb05 (First commit)
   const loop = useCallback(() => {
     if (isRunning) {
       rafRef.current = requestAnimationFrame(loop);
@@ -47,6 +71,15 @@ export const useTimeline = () => {
           return ++prevCount;
         }
 
+<<<<<<< HEAD
+=======
+        if (recording === RECORDING_STATUS.RECORDING) {
+          setIsRunning(false);
+          setRecording(RECORDING_STATUS.RECORDED);
+          return totalFrames;
+        }
+
+>>>>>>> ac3eb05 (First commit)
         return 0;
       });
     }
